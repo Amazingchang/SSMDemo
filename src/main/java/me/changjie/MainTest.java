1d25ce358dff42344bba7c7bb399ac5da8b1e4db
changjie <doupi.cj@raycloud.com>
2018-04-20 17:00:38 +0800

    log配置

---
+++ src/main/java/me/changjie/MainTest.java
@@ -0,0 +1,23 @@
+package me.changjie;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class MainTest {
+
+    private static Logger log = LoggerFactory.getLogger(MainTest.class);
+
+    /**
+     * root和logger(category)是父子关系
+     * logger节点存在时会覆盖root节点
+     * 当logger节点不存在时会使用root节点配置
+     * @param args
+     */
+    public static void main(String[] args) {
+        log.debug("1111111");
+
+        log.info("22222222");
+
+        log.error("3333333");
+    }
+}
